a6bea0ea7b8046a031a46087b20046386f2b7519
Yasuhito Nishide <yasuhitonishide@gmail.com>
2021-03-11 22:35:43 +0900

    no message

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Folder;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+    public function index()
+    {
+        $folders = Folder::all();
+
+        return view('tasks/index',[
+            'foders' => $folders,
+        ]);
+    }
+}
