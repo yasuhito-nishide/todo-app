7acc6c462e1fcafe160fac20aaec9e79a4c14884
Yasuhito Nishide <yasuhitonishide@gmail.com>
2021-03-14 10:33:12 +0900

    フォルダ入力フォーム作成　バリデーション実装

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Folder;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateFolder;
 
 class FolderController extends Controller
 {
@@ -13,7 +14,7 @@ class FolderController extends Controller
         return view('folders/create');
     }
 
-    public function create(Request $request)
+    public function create(CreateFolder $request)
     {
         // フォルダモデルのインスタンスを作成する
         $folder = new Folder();
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
-Route::get('/folders/create','FolderController@showCreateForm')->name('folder.create');
+Route::get('/folders/create','FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create','FolderController@create');
