19c7c949803fcbfd2227907b4025f8ef5880dc3e
Yasuhito Nishide <yasuhitonishide@gmail.com>
2021-03-21 23:15:21 +0900

    no message

--- database/migrations/2021_03_10_143321_create_folders_table.php
+++ database/migrations/2021_03_10_143321_create_folders_table.php
@@ -15,7 +15,7 @@ class CreateFoldersTable extends Migration
     public function up()
     {
         Schema::create('folders', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('user_id');
             $table->string('title',20);
             $table->timestamps();
         });
